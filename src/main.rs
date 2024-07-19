ac9ee98d23ac288fbe4d78a27bb8913f92dcfb3e
Andrew Arrow <oneone@gmail.com>
2024-07-19 11:56:44 -0700

    added support for new API version

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use clap::{Arg, Command};
+use clap::{Parser, Subcommand};
+use std::env;
+use std::time::Duration as StdDuration;
+use tokio::time::{sleep, Duration};
+
+#[tokio::main]
+async fn main() {
+    let matches = Command::new("Toggl CLI")
+        .version("1.0")
+        .author("Andrew Arrow <aa@andrewarrow.dev>")
+        .about("add time to toggle from terminal")
+        .arg(
+            Arg::new("DESC")
+                .help("what you did")
+                .required(true)
+                .index(1),
+        )
+        .get_matches();
+
+    println!("hi");
+}
