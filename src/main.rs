c2f3cfc4b5ccedfb651615e84e82dce572860cf5
Andrew Arrow <oneone@gmail.com>
2024-07-19 17:34:33 -0700

    fixed broken pagination

--- src/main.rs
+++ src/main.rs
@@ -13,29 +13,37 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .arg(
             Arg::new("DESC")
                 .help("what you did")
-                .required(true)
+                .required(false)
                 .long("desc")
                 .value_parser(clap::value_parser!(String)),
         )
         .arg(
             Arg::new("TASK")
                 .help("which task?")
-                .required(true)
+                .required(false)
                 .long("task")
                 .value_parser(clap::value_parser!(String)),
         )
         .get_matches();
 
-    let queryCap = matches.get_one::<String>("TASK").expect("TASK is required");
-    let query_lower = queryCap.to_lowercase();
-    let query: &str = &query_lower;
+    let queryCap = matches.get_one::<String>("TASK");
+    let descTxt = matches.get_one::<String>("DESC");
+    if queryCap.trim().is_empty() == "" && descTxt.trim().is_empty() {
+        println!("--task=prefix or --desc='desc of what I did'");
+        return;
+    }
 
-    let tasks_by_project = tasks::fetch_tasks().await?;
+    if queryCap.trim().is_empty() != "" {
+        let query_lower = queryCap.to_lowercase();
+        let query: &str = &query_lower;
 
-    let search_results = tasks::search_tasks(&tasks_by_project, query);
+        let tasks_by_project = tasks::fetch_tasks().await?;
 
-    for task in search_results {
-        println!("{:#?}", task);
+        let search_results = tasks::search_tasks(&tasks_by_project, query);
+
+        for task in search_results {
+            println!("{:#?}", task);
+        }
     }
 
     Ok(())
